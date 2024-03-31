d6d0b0d8bd121b41afcdb2cea493aa20d0cdb8e2
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-03-31 17:39:44 -0400

    Time: 2 ms (97.83%) | Memory: 44.9 MB (42.63%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,19 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    System.out.println("Indices: " + i + ", " + j);
-                    // Returning an array with indices i and j
-                    return new int[]{i, j};
-                }
+         //creating a hashmap
+        Map<Integer, Integer> hashmap = new HashMap<>();
+
+        for (int i=0;i<nums.length;i++){
+
+            //get the complment using the target value
+            int diff = target - nums[i];
+
+            //search the hashmap for the complement if found, we got our pair
+
+            if(hashmap.containsKey(diff)){
+
+                return new int[]{hashmap.get(diff),i};
             }
+
+            //put the element  in hashmap for subsequent searches
+
+            hashmap.put(nums[i],i);
+
         }
-        // If no solution found
-        System.out.println("No solution found.");
-        // Returning an empty array as no solution is found
-        return new int[0];
-        }
-        
+        return new int[]{}; // No solution found
+    }
+
     }
